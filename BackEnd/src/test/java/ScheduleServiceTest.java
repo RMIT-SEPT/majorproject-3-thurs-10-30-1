90d8416d6d64f153a5f2e836e62b06927d5805e4
Lucas Perlind <perlindluca@gmail.com>
2020-08-16 21:38:44 +1000

    Add service test class

---
+++ BackEnd/src/test/java/ScheduleServiceTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import schedule.microservice.ScheduleServiceMicro;
+import schedule.model.service.*;
+import schedule.repositories.ServiceRepo;
+import schedule.web.UserController;
+
+@SpringBootTest
+class ScheduleServiceTest {
+
+    ScheduleServiceMicro serviceMicro = new ScheduleServiceMicro();
+
+}
