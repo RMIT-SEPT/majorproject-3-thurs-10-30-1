14b4a463c32f7841fedb343fe6b350b65521d580
[Michael Sartorel s3786267] <[s3786267@student.rmit.edu.au]>
2020-08-20 11:07:21 +1000

    Added service name to service class

--- BackEnd/src/main/java/schedule/model/service/ScheduleService.java
+++ BackEnd/src/main/java/schedule/model/service/ScheduleService.java
@@ -10,6 +10,9 @@ public class ScheduleService
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long serviceId;
 
+    @NotBlank(message = "Service name is required")
+    private String name;
+
     @NotBlank(message = "Description is required")
     private String description;
 
@@ -17,9 +20,10 @@ public class ScheduleService
 
     public ScheduleService() {}
 
-    public ScheduleService(Long serviceId, String description, TimeAvailability availability)
-    {
+    public ScheduleService(Long serviceId, @NotBlank(message = "Service name is required") String name,
+            @NotBlank(message = "Description is required") String description, TimeAvailability availability) {
         this.serviceId = serviceId;
+        this.name = name;
         this.description = description;
         this.availability = availability;
     }
