3f62621d17a84c881681dace119cf2166041e109
s3769993 <s3769993@student.rmit.edu.au>
2020-08-23 13:49:13 +1000

    Begin front-end programming. Make a simple menu bar which can be fed a custom array of links. Begin working on login component.

---
+++ FrontEnd/scheduler/src/Components/AGMEnav.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+class AGMEnav extends Component
+{
+    render()
+    {
+       let customLinks = this.props.links.map((link,index) =>
+        {
+            return(
+                <Nav.Link href={link.link}> {link.label}</Nav.Link>
+            );
+        });
+
+        return(
+        <Navbar bg ="darkBlue" expand="lg">
+                <Navbar.Brand href="#home">AGME</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    {customLinks}
+
+                    <NavDropdown title="Settings" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    {/*<Form inline>*/}
+                    {/*    <FormControl type="text" placeholder="Search" className="mr-sm-2" />*/}
+                    {/*    <Button variant="outline-success">Search</Button>*/}
+                    {/*</Form>*/}
+                </Navbar.Collapse>
+            </Navbar>
+
+        )
+    }
+
+}
+
+export default AGMEnav;
---
+++ FrontEnd/scheduler/src/Components/aboutUs.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+class aboutUs extends Component {
+    render()
+    {
+        return(
+            <div><p>"I will contain the left side of this thing"</p></div>
+        )
+    }
+}
+
+export default aboutUs;
