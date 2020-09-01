eda9f6e74ed967828eb5881a6da99cb70359716f
Ali <s3661626@student.rmit.edu.au>
2020-09-01 17:30:02 +1000

    Dashboard dropdown meau
    The dropdown meau doenst work right at the moment

--- FrontEnd/scheduler/src/Components/BookingCreator.js
+++ FrontEnd/scheduler/src/Components/BookingCreator.js
@@ -10,9 +10,16 @@ class BookingCreator extends Component
 
     return (
         <div className = "bookingCreator">
-         i am a booking cfa 
-         <DarkButton label={label} />
+        
+        <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+        <div id="myDropdown" class="dropdown-content">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
         </div>
+
+         <DarkButton label={label} />
+         </div>
     )
 }
 }
