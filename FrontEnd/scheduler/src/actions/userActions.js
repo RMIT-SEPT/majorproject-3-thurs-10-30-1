40a4365a6f29b1f03b9e88e435151428bce278bc
[Michael Sartorel s3786267] <[s3786267@student.rmit.edu.au]>
2020-09-22 13:23:54 +1000

    Use public ip address for back-end requests

--- FrontEnd/scheduler/src/actions/userActions.js
+++ FrontEnd/scheduler/src/actions/userActions.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+const localIpUrl = require('local-ip-url');
 
 export const userLogin = async (details) =>
 {
     try
     {
-        const res = await axios.post("http://localhost:8080/api/user/login", details);
+        const res = await axios.post(`http://${localIpUrl()}:8080/api/user/login`, details);
         console.log(res);
         return res.data;
     }
