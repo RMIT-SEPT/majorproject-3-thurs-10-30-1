f8bd273acc2651bad78c0b489ae6682a416cfeca
[Michael Sartorel s3786267] <[s3786267@student.rmit.edu.au]>
2020-10-13 15:35:15 +1100

    Updated BusinessController to return ResponseEntity

--- BackEnd/src/main/java/schedule/web/BusinessController.java
+++ BackEnd/src/main/java/schedule/web/BusinessController.java
@@ -1,6 +1,5 @@
 package schedule.web;
 
-import java.util.*;
 import javax.validation.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.*;
@@ -23,24 +22,26 @@ public class BusinessController {
         if (result.hasErrors()) {
             return new ResponseEntity<>("Invalid Business Object", HttpStatus.BAD_REQUEST);
         }
-        Business newBusiness = businessMicro.saveOrUpdate(business);
-        return new ResponseEntity<>(business, HttpStatus.CREATED);
+        return new ResponseEntity<>(businessMicro.saveOrUpdate(business), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public Business getBusinessById(@PathVariable long id) {
-        return businessMicro.businessExistsById(id) ? businessMicro.getBusinessById(id) : null;
+    public ResponseEntity<?> getBusinessById(@PathVariable long id) {
+        Business business = businessMicro.getBusinessById(id);
+        return business != null ? new ResponseEntity<>(business, HttpStatus.FOUND) :
+            new ResponseEntity<>("Business not found", HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Business> getAllBusinesses() {
-       return businessMicro.getAllBusinesses();
-    }   
+    public ResponseEntity<?> getAllBusinesses() {
+       return new ResponseEntity<>(businessMicro.getAllBusinesses(), HttpStatus.FOUND);
+    }
 
     @GetMapping(value = "/{id}/services")
-    public List<ScheduleService> getBusinessServices(@PathVariable long id)
+    public ResponseEntity<?> getBusinessServices(@PathVariable long id)
     {
         Business business = businessMicro.getBusinessById(id);
-        return business != null ? business.getServices() : null;
+        return business != null ? new ResponseEntity<>(business.getServices(), HttpStatus.FOUND) :
+            new ResponseEntity<>("Business not found", HttpStatus.BAD_REQUEST);
     }
 }
